d192ac6931ce7c65d685d157178e1f6bbc446aad
Zensltn <160093658+Zensltn@users.noreply.github.com>
2024-04-24 01:13:54 +0800

    Add files via upload

--- akademik/perkuliahan/urls.py
+++ akademik/perkuliahan/urls.py
@@ -5,8 +5,11 @@ from . import views
 
 urlpatterns = [
 
-    path('dashboard/',views.dashboard),
-    path('profil/',views.profil),
-
+    path('dashboard/',views.dashboard,name='home'),
+    path('profil/',views.profil,name='manage_profil'),
+    path('matakuliah/',views.matakuliah,name='manage_matakuliah'),
+    path('mahasiswa-baru/',views.mahasiswa,name='manage_mahasiswa'),
+    path('jurusan/',views.Jurusan,name='manage_jurusan'),
+    path('tambah_Jurusan',views.tambah_Jurusan,name='tambah_Jurusan'),
 ]
 
